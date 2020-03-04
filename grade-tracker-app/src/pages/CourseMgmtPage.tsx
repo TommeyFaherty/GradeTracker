0ad16d6909c0df34fc6fec4fa7fe6bd7475d94f4
TommeyFaherty <G00344243@gmit.ie>
2020-03-04 18:47:29 +0000

    css and layout changes

--- grade-tracker-app/src/pages/CourseMgmtPage.tsx
+++ grade-tracker-app/src/pages/CourseMgmtPage.tsx
@@ -1,4 +1,4 @@
-import { IonContent, IonButton, IonHeader,IonMenuButton,IonMenu,IonIcon, IonItem, IonInput, IonLabel, IonList, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonButton, IonHeader,IonMenuButton,IonMenu,IonIcon, IonItem, IonInput, IonLabel, IonList, IonPage, IonTitle, IonToolbar, IonCard, IonCardContent } from '@ionic/react';
 import React, { useState } from 'react'
 import { useSelector } from 'react-redux'
 import { logoutUser } from '../firebaseConfig'
@@ -7,7 +7,12 @@ import {useHistory} from 'react-router';
 const CourseMgmtPage: React.FC = () => {
 
   const username = useSelector((state:any) => state.user.username)
+  const [course, setCourse] = useState(''); 
   const history = useHistory()
+
+  //let element: HTMLElement = document.getElementsByClassName('btn')[0] as HTMLElement;
+  //element.click();
+
   async  function logout(){
     await logoutUser()
      history.replace('./')
@@ -17,13 +22,24 @@ const CourseMgmtPage: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Testing Course Management</IonTitle>
+          <IonTitle>Testing Course Management
+            <IonButton onClick={logout} class="ion-float-right">Logout</IonButton>
+          </IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent>
-
-     <p>hello {username} </p>
-     <IonButton onClick={logout}>Logout</IonButton>
+        <p>hello {username} </p>
+        <IonCard>
+          <IonCardContent> 
+            <IonTitle>Enter your current course:</IonTitle>
+            <IonInput 
+              placeholder="Enter Course" 
+              value={course}
+              onIonChange={(e: any) => setCourse(e.target.value)}
+            />
+            <IonButton rel="">Enter</IonButton>
+          </IonCardContent>
+        </IonCard>
       </IonContent>
     </IonPage>
   );
--- grade-tracker-app/src/pages/Home.tsx
+++ grade-tracker-app/src/pages/Home.tsx
@@ -42,7 +42,7 @@ const Home = () => {
       </IonHeader>
 
       <IonContent>
-        <IonCard>
+        <IonCard color="primary">
           <IonHeader>What is Gracker?</IonHeader>
           <IonCardContent>
             Gracker is a web-app designed to help students keep track of their grades throughout their academic terms.
@@ -50,6 +50,23 @@ const Home = () => {
           </IonCardContent>
         </IonCard>
   
+        <IonCard color="medium">
+          <IonHeader >Why would I need this?</IonHeader>
+          <IonCardContent>
+            Gracker is here to help you stay organised. It allows a space for you to access your results easily and 
+            consistently. Being able to track your academic highs and lows can help students understand where they need 
+            to focus more and could be the visual motivator needed. 
+          </IonCardContent>
+        </IonCard>
+
+        <IonCard color="medium">
+          <IonHeader>How do I use it?</IonHeader>
+          <IonCardContent>
+            Simply click on the log in button in the top right. Then click the link to create a new account and you 
+            can get started immediately!  
+          </IonCardContent>
+        </IonCard>
+
       </IonContent>
     </IonPage>
   );
